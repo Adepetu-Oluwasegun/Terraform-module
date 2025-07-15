feddf631a5118b35d28d6e0e1093a2824611556d
Oluwasegun Adepetu <Oluwasegunadepetu@gmail.com>
2025-07-15 16:59:26 +0100

    update

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -82,7 +82,7 @@ resource "aws_ecs_service" "ecs_service" {
 
   # vpc and security groups
   network_configuration {
-    subnets                 = []
+    subnets                 = [aws_subnet.private_app_subnet_az1.id, aws_subnet.private_app_subnet_az2.id]
     security_groups         = [aws_security_group.ecs_security_group.id] 
     assign_public_ip        = false
   }
