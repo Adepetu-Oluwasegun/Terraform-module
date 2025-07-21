a8e706e24c4e74acb725501f3688a78a3833e8d3
Oluwasegun Adepetu <Oluwasegunadepetu@gmail.com>
2025-07-21 14:43:26 +0100

    new update

--- modules/ecs/variables.tf
+++ modules/ecs/variables.tf
@@ -1,2 +1,7 @@
 variable "project_name" {}
 variable "container_image" {}
+variable execution_role_arn {}
+variable "private_app_subnet_az1_id" {}
+variable "private_app_subnet_az2_id" {}
+variable "ecs_security_group_id" {}
+variable "target_group_arn" {}
