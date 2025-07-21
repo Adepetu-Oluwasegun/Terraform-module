76e35ae11681b0792337ec09524209f97c11cb40
Oluwasegun Adepetu <Oluwasegunadepetu@gmail.com>
2025-07-21 14:47:55 +0100

    update

--- modules/acm/variables.tf
+++ modules/acm/variables.tf
@@ -1,3 +1,5 @@
 variable "domain_name" {}
 variable "alternative_name" {}
 variable "hosted_zone_id" {}
+variable "application_load_balancer_dns_name" {}
+variable "application_load_balancer_zone_id" {} 
